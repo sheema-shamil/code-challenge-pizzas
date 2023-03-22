1030a3100ea4be05aea32babd65b908fe505c01a
sheema <sheemashamilfarook@gmail.com>
2023-03-22 09:19:34 +0300

    completed

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,7 +6,7 @@
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
 r1 = Restaurant.create(name: 'Sottocasa NYC', address: '298 Atlantic Ave, Brooklyn, NY 11201')
-r2 = Restaurant.create(name: 'PizzArte', address: '69 W 55th St, New York, NY 10019')
+r2 = Restaurant.create(name: 'PizzHut', address: '50 west 55th St, New York, NY 10019')
 
 p1 = Pizza.create(name: 'Cheese', ingredients: 'Dough, Tomato Sauce, Cheese')
 p2 = Pizza.create(name: 'Pepperoni', ingredients: 'Dough, Tomato Sauce, Cheese, Pepperoni')
